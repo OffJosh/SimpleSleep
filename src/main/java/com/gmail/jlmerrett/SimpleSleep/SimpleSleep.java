234410e87c38b2a22f0c356782b0a564cbc397df
BuildTools <unconfigured@null.spigotmc.org>
2020-06-29 16:44:37 +0100

    init commit

---
+++ src/main/java/com/gmail/jlmerrett/SimpleSleep/SimpleSleep.java
@@ -0,0 +1,16 @@
+package com.gmail.jlmerrett.SimpleSleep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleSleep extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
